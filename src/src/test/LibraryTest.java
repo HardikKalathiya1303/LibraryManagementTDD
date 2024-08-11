b79bc8c27d153b76f7c5f657812e8da2b21b8140
hppatel007 <hardikpkalathiya@gmail.com>
2024-08-11 13:25:59 +0530

    Add static arrays for authors and titles to LibraryTest class

---
+++ src/src/test/LibraryTest.java
@@ -0,0 +1,15 @@
+package src.test;
+
+class LibraryTest {
+    private static final String[] AUTHORS = {
+            "Bhavik Sarkhedi", "Rabindranath Tagore", "Salman Rushdie",
+            "Arundhati Roy", "R.K. Narayan", "Jhumpa Lahiri",
+            "Mulk Raj Anand", "Ruskin Bond", "Chetan Bhagat", "Vikram Seth"
+    };
+    private static final String[] TITLES = {
+            "The Immortals of Meluha", "The Secret of the Nagas",
+            "The Oath of the Vayuputras", "Mahabharata",
+            "The Great War of Hind", "Ramayana",
+            "The Palace of Illusions", "Jaya"
+    };
+}
