2bf8b688f8dc4acf2f37481d0ac9526e75db37b4
hppatel007 <hardikpkalathiya@gmail.com>
2024-08-11 13:19:39 +0530

    Add Book class with initial attributes and constructor

--- src/src/main/Book.java
+++ src/src/main/Book.java
@@ -12,24 +12,4 @@ public class Book {
         this.author = author;
         this.isAvailable = true; // Assume a new book is available by default
     }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public void setAvailable(boolean available) {
-        isAvailable = available;
-    }
 }
